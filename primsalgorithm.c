2f393163f6f20bffc73281e777819a871fcc0c81
laitchs <lshen01@college.harvard.edu>
2014-09-16 21:34:21 -0400

    Commenting prims

--- primsalgorithm.c
+++ primsalgorithm.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-float Prim(vertex *q, g)
+float Prim(vertex *q, float **g)
 {
+	//Take seed to be the first vertex in heap array. Change its distance from the tree to be 0.
 	deckey(q, 0, 0);
+	//delete this vertex from "v-s"
 	delmin(q);
 
+	//initialize the queue size to the number of remaining vertices.
 	int qsize= m-1;
 	
+	//initialize the weight of the MST so far to 0.
 	float weight=0;
 
+	//while the heap isn't empty, delete the minimum element and update the
+	// remaining vertices distances from the working tree S
 	while (!isEmpty (q))
 	{
 		struct vertex u = delmin(q);
@@ -27,6 +33,7 @@ float Prim(vertex *q, g)
 	return weight;
 }	
 
+//change the distance of the vertex at index to newDist
 void deckey(q, int index, float newDist)
 {
 	vertex v=q[index];	
