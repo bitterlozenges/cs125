05df3a2e4aff7433c4976e4d0772614086b6988c
laitchs <lshen01@college.harvard.edu>
2014-09-16 22:50:27 -0400

    Edited prims to work with queue.c

--- primsalgorithm.c
+++ primsalgorithm.c
@@ -1,42 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-float Prim(vertex *q, float **g)
+float Prim(queue* q, float **g)
 {
 	//Take seed to be the first vertex in heap array. Change its distance from the tree to be 0.
-	deckey(q, 0, 0);
+	decKey(q, 0, 0);
 	//delete this vertex from "v-s"
-	delmin(q);
-
-	//initialize the queue size to the number of remaining vertices.
-	int qsize= m-1;
+	delMin(q);
 	
 	//initialize the weight of the MST so far to 0.
 	float weight=0;
 
 	//while the heap isn't empty, delete the minimum element and update the
 	// remaining vertices distances from the working tree S
-	while (!isEmpty (q))
+	while (q->last >=0)
 	{
-		struct vertex u = delmin(q);
-		weight = u.dist;
-		for (i=0; i<qsize; i++)
+		vertex u = delMin(q);
+		weight += u.dist;
+		for (i=0; i<q->last; i++)
 		{
-			struct vertex v=q[i];
-			int distFromU = g[v.val, u.val];
+			vertex v=(q->heap)[i];
+			int distFromU = g[v.val][u.val];
 			if(distFromU < v.dist)
-				deckey(q,i, distFromU);
+				decKey(q,i, distFromU);
 		}
-		heapify(q);
-		qsize--;
+		buildHeap(q);
 	}
 	return weight;
 }	
 
 //change the distance of the vertex at index to newDist
-void deckey(q, int index, float newDist)
+void deckey(queue* q, int index, float newDist)
 {
-	vertex v=q[index];	
+	vertex v=(q->heap)[index];	
 	vertex u={newDist, v.val};
-	q[index]= u;
+	(q->heap)[index]= u;
 }
