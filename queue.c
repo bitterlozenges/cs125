4fb3f3dc13aeb4416c290702537e191e145f9317
andrewwongrolle <awongrolle@college.harvard.edu>
2014-09-19 01:16:17 -0400

    fixed heaps to deal with index values

--- queue.c
+++ queue.c
@@ -37,26 +37,28 @@ queue init(int n, float** verts, float cutoff)
 //TODO: THIS SEEMS BAD PUT ON TOP YO
 void heapify(queue* Q, int index);
 
+// swaps two elements in the heap at indices v1 and v2 
 void swap(queue* Q, int v1, int v2)
 {
-  vertex* temp = Q->heap[v1];
+  int temp = Q->heap[v1];
   Q->heap[v1] = Q->heap[v2];
   Q->heap[v2] = temp;
 }
 
 // deletes the root of the heap and returns the vertex the root points to
-vertex delMin(queue* Q)
+vertex delMin(queue* Q, vertex* G)
 {
-  vertex* min = (Q->heap)[0];
+  vertex min = G[(Q->heap)[0]];
   // passes into swap the address of the root of Q and the last leaf
   // swap(&(Q->heap)[0],&(Q->heap)[Q->last]);
   swap(Q, 0, Q->last);
   Q->last -= 1;
-  heapify(Q,0);
-  return *min;
+  heapify(Q, G, 0);
+  return min;
 }
 
-void heapify(queue* Q, int index)
+// balances heap assuming only the root violates the heap invariant
+void heapify(queue* Q, vertex* G, int index)
 {
   // return if heap is empty
   if (Q->last < 0)
@@ -67,17 +69,17 @@ void heapify(queue* Q, int index)
   int current = index;
 
   // if our vertex has children and the left child is smaller
-  if (left <= Q->last && (Q->heap)[left].dist < (Q->heap)[current].dist)
-  if (left <= Q->last && (Q->heap)[left]->distFromS < (Q->heap)[current]->distFromS)
+  if (left <= Q->last && G[Q->heap[left]]->distFromS < G[Q->heap[current]]->distFromS)
+  // if (left <= Q->last && (Q->heap)[left]->distFromS < (Q->heap)[current]->distFromS)
     current = left;
   // if vertex has children and the right child is smaller
-  if (right <= Q->last && (Q->heap)[right]->distFromS < (Q->heap)[current]->distFromS)
+  if (right <= Q->last && G[(Q->heap)[right]]->distFromS < G[(Q->heap)[current]]->distFromS)
     current = right;
   // if we found a smaller value, put it at the top 
   if (current != index)
   {
     swap(Q, current, index);
-    heapify(Q,current);
+    heapify(Q,G,current);
   }
   // if no switches were made, our heap is balanced
   return;
